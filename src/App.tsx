609dcf9b1378661ac1e76e5b233733dba5cca13c
andremizuno <andremizuno@gmail.com>
2023-09-03 14:51:48 -0300

    aplicando minha estrutura

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaView, Text, View } from "react-native"
+
+const App = () => {
+
+    return(
+        <SafeAreaView>
+            <Text>Testando</Text>
+        </SafeAreaView>
+    )
+};
+
+export default App;
